03f5b32b2ed1012ba333d736644c31da60b08727
PatoGM <patriciogm@live.com>
2020-09-05 21:32:41 -0500

    Adding in test app
    Adding in mock-up app, app was made in the style of Corey Schafer's Flask tutorial:
    https://www.youtube.com/watch?v=MwZwr5Tvyxo
    
    Will attempt to deploy to Heroku

---
+++ Py_Flask_Test/Flask_Practice.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)   # __name__ is the name of the module, flask knows where to look for templates/static files
+
+
+posts = [
+    {
+        'author': 'Site/Post Author',
+        'title': 'Basic Site Info',
+        'content': 'Instructions and other info regarding this site',
+        'date_posted': 'September 5, 2020'
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+# @app.route("/about")
+# def about():
+#     return render_template('about.html', title='About')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# cd COSC_4351
+# set FLASK_APP=Flask_Practice.py
+# set FLASK_ENV=development
+# flask run
